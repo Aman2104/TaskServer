e1e7af5e498eadf2f8c33806457cf26746e1aa9b
Aman2104 <amanyara88@gmail.com>
2024-03-20 19:54:39 +0530

    vercel added

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     database: process.env.mysql_database,
     user: process.env.mysql_user,
     password: process.env.mysql_password,
-    port:3306
+    port:process.env.mysql_host
 
 });
 
